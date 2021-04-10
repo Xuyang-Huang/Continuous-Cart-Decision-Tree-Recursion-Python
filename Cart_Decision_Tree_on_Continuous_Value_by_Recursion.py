4d36c62d2685f575dd6adb6b0bbcb8e1c7bd2ebf
test <test@test.com>
2021-04-11 02:29:17 +0800

    Fix bugs

--- Cart_Decision_Tree_on_Continuous_Value_by_Recursion.py
+++ Cart_Decision_Tree_on_Continuous_Value_by_Recursion.py
@@ -8,9 +8,6 @@
 import numpy as np
 import sklearn.datasets as sk_dataset
 
-minimum_leaf = 4
-
-
 class TreeNode:
     """A decision tree node
 
@@ -38,7 +35,7 @@ class TreeNode:
         :param label: A 1-D Numpy array.
         :return: [left split data, right split data], [left split data ground truth, right split data ground truth]
         """
-        left_mask = data[:, self.feature_index] < self.thr
+        left_mask = data[:, self.feature_index] <= self.thr
         right_mask = ~left_mask
 
         left_leaf_data = data[left_mask]
@@ -59,7 +56,7 @@ class TreeNode:
                  [left split data index, right split data index]
                  [left split data ground truth, right split data ground truth]
         """
-        left_mask = data[:, self.feature_index] < self.thr
+        left_mask = data[:, self.feature_index] <= self.thr
         right_mask = ~left_mask
 
         left_leaf_data = data[left_mask]
@@ -86,16 +83,17 @@ class CartDecisionTree:
         __pruning: Bool, if True prune the DT, or not.
 
     """
-    def __init__(self, min_leaf, split_method='gini', pruning_prop=0.2, pruning=False):
+    def __init__(self, min_leaf, split_method='gini', pruning_prop=None, n_try=None):
         self.root = None
         self.__min_leaf = min_leaf
         self.__n_class = None
         c = Criterion()
         self.__train_node = getattr(c, split_method)
         self.__pruning_prop = pruning_prop
-        self.__pruning = pruning
+        self.__pruning = pruning_prop is not None
+        self.__n_try = n_try
 
-    def train(self, data, label, n_class):
+    def train(self, data, label, n_class, pruning_data=None, pruning_label=None):
         """Train a decision tree.
 
         Using recursion to train a Cart DT.
@@ -106,37 +104,39 @@ class CartDecisionTree:
         :return: No return.
         """
         self.__n_class = n_class
+        assert (pruning_label is None) == (pruning_data is None), 'Please provide data and label both or not!'
         if self.__pruning:
-            pruning_data = data[:int(len(data) * self.__pruning_prop)]
-            pruning_label = label[:int(len(label) * self.__pruning_prop)]
-            data = data[int(len(data) * self.__pruning_prop):]
-            label = label[int(len(label) * self.__pruning_prop):]
+            if pruning_data is None:
+                pruning_data = data[:int(len(data) * self.__pruning_prop)]
+                pruning_label = label[:int(len(label) * self.__pruning_prop)]
+                data = data[int(len(data) * self.__pruning_prop):]
+                label = label[int(len(label) * self.__pruning_prop):]
 
         def grow(_data, _label):
             # Train single node.
-            _feature_index, _thr = self.__train_node(_data, _label)
+            _feature_index, _thr = self.__train_node(_data, _label, self.__n_try)
             _node = TreeNode(_feature_index, _thr)
             _split_data, _split_label = _node.split(_data, _label)
 
-            if (len(_split_label[0]) == 0) | (len(_split_label[1]) == 0):
+            if len(_split_label[0]) == 0:
                 return None
-            # Regard most class as the class at the leaf.
-            _node.left_class = np.argmax(np.bincount(_split_label[0]))
-            _node.right_class = np.argmax(np.bincount(_split_label[1]))
+            elif len(_split_label[0]) < self.__min_leaf:
+                _node.left_class = np.argmax(np.bincount(_split_label[0]))
+            elif (_split_label[0] == _split_label[0][0]).all():
+                _node.left_class = np.argmax(np.bincount(_split_label[0]))
+            else:
+                _node.left = grow(_split_data[0], _split_label[0])
+                _node.left_class = np.argmax(np.bincount(_split_label[0]))
 
-            if len(_split_label[0]) < self.__min_leaf:
-                return _node
-            if (_split_label[0] == _split_label[0][0]).all():
-                return _node
-
-            _node.left = grow(_split_data[0], _split_label[0])
-
-            if len(_split_label[1]) < self.__min_leaf:
-                return _node
-            if (_split_label[1] == _split_label[1][0]).all():
-                return _node
-
-            _node.right = grow(_split_data[1], _split_label[1])
+            if len(_split_label[1]) == 0:
+                return None
+            elif len(_split_label[1]) < self.__min_leaf:
+                _node.right_class = np.argmax(np.bincount(_split_label[1]))
+            elif (_split_label[1] == _split_label[1][0]).all():
+                _node.right_class = np.argmax(np.bincount(_split_label[1]))
+            else:
+                _node.right = grow(_split_data[1], _split_label[1])
+                _node.right_class = np.argmax(np.bincount(_split_label[1]))
 
             return _node
 
@@ -165,9 +165,8 @@ class CartDecisionTree:
                 else:
                     tmp_acc = 0
                 root.left_acc = tmp_acc
-                return None
-
-            __inference(root.left, _split_data[0], _split_data_index[0], _split_label[0])
+            else:
+                __inference(root.left, _split_data[0], _split_data_index[0], _split_label[0])
 
             if root.right is None:
                 if len(_split_data[1]) != 0:
@@ -175,13 +174,12 @@ class CartDecisionTree:
                 else:
                     tmp_acc = 0
                 root.right_acc = tmp_acc
-                return None
+            else:
+                __inference(root.right, _split_data[1], _split_data_index[1], _split_label[1])
 
-            __inference(root.right, _split_data[1], _split_data_index[1], _split_label[1])
-
-            if (root.right_acc is not None) & (root.right_acc is not None):
+            if (root.left_acc is not None) & (root.right_acc is not None):
                 child_acc = np.mean([root.right_acc, root.left_acc])
-                if root.acc > child_acc:
+                if root.acc >= child_acc:
                     del root.left
                     del root.right
                     root.left, root.right, root.left_acc, root.right_acc = None, None, None, None
@@ -202,16 +200,14 @@ class CartDecisionTree:
             if root.left is None:
                 if len(_split_data[0]) != 0:
                     result[root.left_class].extend(_split_data_index[0])
-                return None
-
-            __inference(root.left, _split_data[0], _split_data_index[0])
+            else:
+                __inference(root.left, _split_data[0], _split_data_index[0])
 
             if root.right is None:
                 if len(_split_data[1]) != 0:
                     result[root.left_class].extend(_split_data_index[1])
-                return None
-
-            __inference(root.right, _split_data[1], _split_data_index[1])
+            else:
+                __inference(root.right, _split_data[1], _split_data_index[1])
 
             return None
         __inference(self.root, data, index)
@@ -234,17 +230,15 @@ class CartDecisionTree:
                 if len(_split_data[0]) != 0:
                     result[root.left_class].extend(_split_data_index[0])
                     result_label[root.left_class].extend(_split_label[0])
-                return None
-
-            __inference(root.left, _split_data[0], _split_data_index[0], _split_label[0])
+            else:
+                __inference(root.left, _split_data[0], _split_data_index[0], _split_label[0])
 
             if root.right is None:
                 if len(_split_data[1]) != 0:
-                    result[root.left_class].extend(_split_data_index[1])
-                    result_label[root.left_class].extend(_split_label[1])
-                return None
-
-            __inference(root.right, _split_data[1], _split_data_index[1], _split_label[1])
+                    result[root.right_class].extend(_split_data_index[1])
+                    result_label[root.right_class].extend(_split_label[1])
+            else:
+                __inference(root.right, _split_data[1], _split_data_index[1], _split_label[1])
 
             return None
         __inference(self.root, data, index, label)
@@ -259,25 +253,33 @@ class CartDecisionTree:
 
 
 class Criterion:
-    def gini(self, data, label):
+    def gini(self, data, label, n_try=None):
         """Traverse all features and value, find the best split feature and threshold.
 
         :param data: A 2-D Numpy array.
         :param label: A 1-D Numpy array.
         :return: Best feature to split, best threshold to split.
         """
+        if n_try is not None:
+            rand_feature_index = np.arange(data.shape[1])
+            np.random.shuffle(rand_feature_index)
+            rand_feature_index = rand_feature_index[:n_try]
+            data = data[:, rand_feature_index]
         best_gini = np.inf
         for i in range(data.shape[1]):
+            sort_index = np.argsort(data[:, i])
+            sub_data = data[sort_index]
+            sub_label = label[sort_index]
             for j in range(1, data.shape[0]):
-                tmp_gini_value = j / len(data) * self.__gini(label[:j]) + \
-                                 (len(data) - j) / len(data) * self.__gini(label[j:])
+                tmp_gini_value = j / len(data) * self.__gini(sub_label[:j]) + \
+                                 (len(data) - j) / len(data) * self.__gini(sub_label[j:])
                 if tmp_gini_value < best_gini:
                     best_gini = tmp_gini_value
-                    best_thr = np.mean([data[j-1, i], data[j, i]])
-                    best_feature = i
+                    best_thr = np.mean([sub_data[j-1, i], sub_data[j, i]])
+                    best_feature = rand_feature_index[i]
         return best_feature, best_thr
 
-    def entropy(self, data, label):
+    def entropy(self, data, label, n_try=None):
         """Traverse all features and value, find the best split feature and threshold.
 
         Find the gain higher than average, pick the highest gain ratio one.
@@ -289,10 +291,17 @@ class Criterion:
         gain = []
         gain_ratio = []
         ent_before = self.__ent(label)
+        if n_try is not None:
+            rand_feature_index = np.arange(data.shape[1])
+            np.random.shuffle(rand_feature_index)
+            rand_feature_index = rand_feature_index[:n_try]
+            data = data[:, rand_feature_index]
         for i in range(data.shape[1]):
+            sort_index = np.argsort(data[:, i])
+            sub_label = label[sort_index]
             for j in range(1, data.shape[0]):
                 tmp_gain = ent_before - \
-                           (j / len(data) * self.__ent(label[:j]) + (len(data) - j) / len(data) * self.__ent(label[j:]))
+                           (j / len(data) * self.__ent(sub_label[:j]) + (len(data) - j) / len(data) * self.__ent(sub_label[j:]))
                 tmp_gain_ratio = tmp_gain / (- j / len(data) * np.log2(j / len(data)) -
                                              (len(data) - j) / len(data) * np.log2((len(data) - j) / len(data)))
                 gain.append(tmp_gain)
@@ -302,17 +311,14 @@ class Criterion:
         gain_ratio[gain < np.mean(gain)] = -np.inf
         best_index = np.argmax(gain_ratio)
         mat_index = np.unravel_index(best_index, [data.shape[1], data.shape[0] - 1])
-        best_feature = mat_index[0]
-        best_thr = np.mean([data[mat_index[1], best_feature], data[mat_index[1] + 1, best_feature]])
+        best_feature = rand_feature_index[mat_index[0]]
+        sub_data = np.sort(data[:, mat_index[0]])
+        best_thr = np.mean([sub_data[mat_index[1]], sub_data[mat_index[1] + 1]])
         return best_feature, best_thr
 
     def __gini(self, label):
         _label_class = list(set(label))
         gini_value = 1 - np.sum([(np.sum(label == i) / len(label)) ** 2 for i in _label_class])
-        return gini_value
-
-    def __gain(self, label, _n_class):
-        gini_value = 1 - np.sum([(np.sum(label == i) / len(label)) ** 2 for i in range(_n_class)])
         return gini_value
 
     def __ent(self, label):
@@ -341,8 +347,10 @@ def prepare_data(proportion):
 
 
 if __name__ == '__main__':
+    minimum_leaf = 1
     train, val, num_class = prepare_data(0.8)
-    cart_dt = CartDecisionTree(minimum_leaf, 'gini')
+    num_try = int(np.sqrt(train[0].shape[1]))
+    cart_dt = CartDecisionTree(minimum_leaf, 'gini', pruning_prop=0.1, n_try=num_try)
     cart_dt.train(train[0], train[1], num_class)
     _, _, train_acc = cart_dt.eval(train[0], train[1])
     pred, pred_gt, val_acc = cart_dt.eval(val[0], val[1])
